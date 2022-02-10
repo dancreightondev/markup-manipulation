34a4b487ac17833def5ca184bb2f1b14e1531edf
Dan Creighton <22957063+dancreightondev@users.noreply.github.com>
2022-02-10 11:58:19 +0000

    bring code format in line with `html-into-xml.py`

--- py/xml-to-excel.py
+++ py/xml-to-excel.py
@@ -1,81 +1,173 @@
-import os
-import xml.etree.ElementTree as ET
-from pandas import DataFrame
 from bs4 import BeautifulSoup as BS
+import glob
+import os as OS
+import xml.etree.ElementTree as ET
+import pandas as PD
 
-def makeDataFrame(fnameQti, fnamesImg, fnamesAud, numImg, numAud):
-    df = DataFrame({"QTI filename" : fnameQti,
-                   "Image filename(s) in Media Library" : fnamesImg,
-                   "Audio file(s) ID" : fnamesAud,
-                   "Number of images in item" : numImg,
-                   "Number of audio files in item" : numAud},
+def make_data_frame(qti_filename, img_filenames, audio_filenames, img_count, audio_count):
+    
+    # Make the data frame according the the provided arguments
+    data_frame = PD.DataFrame({"QTI filename" : qti_filename,
+                   "Image filename(s) in Media Library" : img_filenames,
+                   "Audio file(s) ID" : audio_filenames,
+                   "Number of images in item" : img_count,
+                   "Number of audio files in item" : audio_count},
                    index=[0])
-    return df
+    
+    # Return the data frame
+    return data_frame
 
-def getImgNamesFromHtmlFile(fname):
-    with open(fname) as html: # Open the provided HTML file
-        content = BS(html, features="html.parser") # Parse the HTML content
-        fnames = [] # Initialise an array to store image filenames
-        for attr in content.find_all("img"): # Iterate through all img objects in the HTML code
-            fnames.append(attr["src"].split("/", 1)[-1]) # Store the filename in the src tag for later
-    return fnames
+def get_image_names_from_html_file(html_filename):
+    
+    # Open the provided HTML file
+    with open(html_filename) as html:
+        
+        # Parse the HTML content
+        content = BS(html, features="html.parser")
 
-def parseXmlFile(file):
-    tree = ET.parse(file) # Parse XML
-    root = tree.getroot() # Get the root of the XML element tree
-    attr = "data" # Object attribute to search for. The objects that link to a file seem to always have the 'data' attribute
-    objs = root.findall(".//*[@{}]".format(attr)) # Find all objects by attribute
-    fnames = []
-    for o in objs: # Iterate through the found objects
-        data = o.attrib[attr] # Get filenames from found objects, as per the specified attribute (attr)
-        fname = data.split("/", 1)[-1] # Tidy up the filename
-        fnames.append(fname) # Store for processing later
-    fnamesHtml = [n for n in fnames if n.endswith(".html")] # Store HTML filenames
-    fnamesImg = [] # Initialise an array to store image filenames
-    for h in fnamesHtml: # Iterate through the HTML files
-        fnamesImg.extend(getImgNamesFromHtmlFile(h)) # Store image filenames
-    fnamesAud = [n for n in fnames if n.endswith(".mp3")] # Store MP3 filenames
-    parsedData = {"QTI filename" : file,
-                  "Image filename(s) in Media Library" : ', '.join(fnamesImg),
-                  "Audio file(s) ID" : ', '.join(fnamesAud),
-                  "Number of images in item" : str(len(fnamesImg)),
-                  "Number of audio files in item" : str(len(fnamesAud))} # Collate parsed data
-    return parsedData
+        # Initialise an array to store image filenames
+        filenames = []
 
-def exportXlsx(df):
-    df.to_excel('qti-export.xlsx', sheet_name='qti.py', index=False) # Save provided pandas DataFrame to qti-export.xlsx in a sheet called qti.py
+        # Iterate through all img objects in the HTML code
+        for attr in content.find_all("img"):
+
+            # Store the filename in the src tag for later
+            filenames.append(attr["src"].split("/", 1)[-1])
+
+    # Return the list of filenames
+    return filenames
+
+def parse_xml_file(xml_filename):
+    
+    # Parse XML
+    tree = ET.parse(xml_filename)
+    
+    # Get the root of the XML element tree
+    root = tree.getroot()
+
+    # Object attribute to search for. The objects that link to a file seem to always have the 'data' attribute
+    attr = "data"
+    
+    # Find all objects by attribute
+    objs = root.findall(".//*[@{}]".format(attr))
+
+    # Initialise an array to store filenames
+    filenames = []
+    
+    # Iterate through the found objects
+    for o in objs:
+
+        # Get filenames from found objects, as per the specified attribute (attr)
+        data = o.attrib[attr]
+
+        # Tidy up the filename
+        filename = data.split("/", 1)[-1]
+
+        # Store for processing later
+        filenames.append(filename)
+
+    # Store HTML filenames
+    html_filenames = [n for n in filenames if n.endswith(".html")]
+
+    # Initialise an array to store image filenames
+    img_filenames = []
+
+    # Iterate through the HTML files
+    for h in html_filenames:
+
+        # Store image filenames
+        img_filenames.extend(get_image_names_from_html_file(h))
+    
+    # Store MP3 filenames
+    audio_filenames = [n for n in filenames if n.endswith(".mp3")]
+
+    # Collate parsed data
+    parsed_data = {"QTI filename" : xml_filename,
+                  "Image filename(s) in Media Library" : ', '.join(img_filenames),
+                  "Audio file(s) ID" : ', '.join(audio_filenames),
+                  "Number of images in item" : str(len(img_filenames)),
+                  "Number of audio files in item" : str(len(audio_filenames))}
+
+    # Return parsed data
+    return parsed_data
+
+def export_to_excel(data_frame, overwrite=False):
+
+    # Set the filename prefix
+    prefix = "xml-to-excel.py_output"
+
+    # If overwriting, ...
+    if overwrite:
+        # Set the filepath
+        outfilepath = OS.getcwd() + "\\" + f"{prefix}.xlsx"
+    
+    # If not overwriting, ...
+    else:
+        # Set a new filename iteratively so any previous files are not overwritten
+        filepaths = glob.glob(f"{prefix}*.xlsx")
+        if len(list(filepaths)) == 0:
+            outfilepath = OS.getcwd() + "\\" + f"{prefix}.xlsx"
+        else:
+            outfilepath = OS.getcwd() + "\\" + f"{prefix}{len(list(filepaths))+1}.xlsx"
+
+    # Export to an Excel file
+    data_frame.to_excel(outfilepath, index=False)
+
+    # Return the filepath it was saved to
+    return outfilepath
 
 def main():
-    inp = "Scan XML files in current directory:\n\n{}\n\nConfirm? (Y/n): "
-    curdir = os.path.dirname(os.path.realpath(__file__))
-    confirm = input(inp.format(curdir))
-    if(confirm.upper() != "Y"):
-        return
-    else:
-        print("\nStarting...\n")
-        sheetData = [] # Initialise an array to store the data to go in the spreadsheet
-        try:
-            print("Parsing files")
-            for file in os.listdir(curdir): # Iterate through the files in the current directory
-                if not file.endswith('.xml'): continue # Only look at the XML files
-                parsedData = parseXmlFile(file) # Parse the XML file and extract the data we need
-                sheetData.append(parsedData) # Save the data we need ready for export
-        except Exception as e:
-            print(e)
-        except:
-            print("An unknown error occurred")
-        else:
-            print("Parsed all files successfully")
-            
-        try:
-            print("Exporting to a new Excel file")
-            exportXlsx(DataFrame(sheetData)) # Export the data to an Excel spreadsheet
-        except Exception as e:
-            print(e)
-        except:
-            print("An unknown error occurred")
-        else:
-            print("Exported successfully")
+    
+    # Initialise an array to store the data to go in the spreadsheet
+    sheet_data = [] 
+    
+    # Attempt to parse the files
+    try:
+        print("Parsing files")
 
-main()
-input("\nPress Enter to exit ")
+        # Iterate through the files in the current directory
+        for file in OS.listdir(OS.getcwd()):
+
+            # Only look at the XML files
+            if not file.endswith('.xml'): 
+                continue
+
+            # Parse the XML file and extract the data we need
+            parsed_data = parse_xml_file(file)
+
+            # Save the data we need ready for export
+            sheet_data.append(parsed_data)
+
+    # Error handling     
+    except Exception as e:
+        print(e)
+    except:
+        print("An unknown error occurred")
+
+    # Print success message if no errors occurred
+    else:
+        print("Parsed all files successfully")
+    
+    # Attempt to export to Excel
+    try:
+        print("Exporting to a new Excel file")
+
+        # Export the data to an Excel spreadsheet
+        export_to_excel(PD.DataFrame(sheet_data))
+
+    # Error handling
+    except Exception as e:
+        print(e)
+    except:
+        print("An unknown error occurred")
+    
+    # Print success message if no errors occurred
+    else:
+        print("Exported successfully")
+
+print(f"This program will look for files in the current directory:\n\n{str(OS.getcwd())}\n")
+confirm = input("Confirm? (Y/n): ")
+if confirm.upper == "Y":
+    print("Starting...\n")
+    main()
+    input("\nPress any key to continue... ")
